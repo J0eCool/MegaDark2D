f2a1dfc24fe8e73279d905675d001d8b8e48e96d
J0eCool <count.j0ecool@gmail.com>
2015-08-12 23:43:19 -0700

    Add bottomless pit kill logic

---
+++ Assets/Scripts/BottomlessPit.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class BottomlessPit : MonoBehaviour, Collideable {
+	public void OnCollide(CollisionData collision) {
+		var health = collision.sender.GetComponent<Health>();
+		if (health) {
+			health.Kill();
+		}
+	}
+}
