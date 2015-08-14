10d30a8d65f3eacbc847a40cf22e2001bc46054e
J0eCool <count.j0ecool@gmail.com>
2015-08-14 00:11:05 -0700

    Add PlayerManager; only activate enemies when close

---
+++ Assets/Scripts/Enemy/ActivateWhenClose.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class ActivateWhenClose : MonoBehaviour {
+	[SerializeField] private float range = 10.0f;
+
+	private List<MonoBehaviour> componentsToActivate = new List<MonoBehaviour>();
+
+	void Start() {
+		var components = GetComponents<MonoBehaviour>();
+		foreach (var component in components) {
+			if (component.enabled && component != this) {
+				component.enabled = false;
+				componentsToActivate.Add(component);
+			}
+		}
+	}
+
+	void Update() {
+		GameObject player = PlayerManager.Instance.Player;
+		Vector3 pos = transform.position;
+		Vector3 playerPos = player.transform.position;
+		Vector3 delta = pos - playerPos;
+		delta.z = 0;
+		bool isInRange = delta.sqrMagnitude < range * range;
+		if (isInRange) {
+			foreach (var component in componentsToActivate) {
+				component.enabled = true;
+			}
+			enabled = false;
+		}
+	}
+}
---
+++ Assets/Scripts/PlayerManager.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class PlayerManager : SingletonComponent<PlayerManager> {
+	public GameObject Player { get; private set; }
+
+	void Start() {
+		Player = GameObject.Find("Player");
+	}
+}
