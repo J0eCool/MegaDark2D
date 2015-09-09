b0784ab5ffc72d6b6db618bad09ce353679d689a
J0eCool <count.j0ecool@gmail.com>
2015-09-09 02:23:26 -0700

    Tileset, upgrade to unity 5.2

--- Assets/Scripts/Facing.cs
+++ Assets/Scripts/Facing.cs
@@ -5,10 +5,16 @@ using System.Collections.Generic;
 public class Facing : JComponent {
 	[SerializeField] private bool facingRight = false;
 
+    [StartComponent] private tk2dSprite sprite = null;
+
 	public bool FacingRight {
 		get { return facingRight; }
 		set { facingRight = value; }
 	}
 
 	public int Dir { get { return facingRight ? 1 : -1; } }
+
+    void Update() {
+        sprite.FlipX = !facingRight;
+    }
 }
