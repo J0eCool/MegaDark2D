9724291119b4bcaa470ab59471b02fa3ff4e8311
J0eCool <count.j0ecool@gmail.com>
2015-09-12 21:45:52 -0700

    Improve player animator

--- Assets/Scripts/Player/PlayerAnimator.cs
+++ Assets/Scripts/Player/PlayerAnimator.cs
@@ -3,6 +3,9 @@ using System.Collections;
 using System.Collections.Generic;
 
 public class PlayerAnimator : JComponent {
+    [SerializeField] private float shootRaiseGunTime = 0.6f;
+    [SerializeField] private float shootEyeShutTime = 0.2f;
+
     [StartComponent] private SpritePhysics physics;
     [StartComponent] private tk2dSpriteAnimator animator;
 
@@ -10,29 +13,66 @@ public class PlayerAnimator : JComponent {
 
     private bool isFiringAnimationPlaying = false;
 
+    private bool isRunning = false;
+    private bool isShooting = false;
+    private bool isEyeShut = false;
+
+    private float shotTimer = -1.0f;
+
     protected override void onStart() {
         input = InputManager.Instance;
 	}
 
-	void Update() {
+	void FixedUpdate() {
+        updateTimers();
+        updateFlags();
+        updateAnimation();
+    }
+
+    private void updateTimers() {
+        shotTimer -= Time.fixedDeltaTime;
+
         if (input.Shoot.DidPress) {
-            animator.Stop();
-            animator.Play("Shoot");
-            isFiringAnimationPlaying = true;
-            animator.AnimationCompleted = (x, y) => {
-                isFiringAnimationPlaying = false;
-                animator.AnimationCompleted = null;
-            };
+            shotTimer = totalShootTime();
+        }
+    }
+
+    private float totalShootTime() {
+        return shootRaiseGunTime + shootEyeShutTime;
+    }
+
+    private void updateFlags() {
+        isRunning = Mathf.Abs(physics.Vel.x) > 0.1f;
+        isShooting = shotTimer > 0.0f;
+        isEyeShut = shotTimer > totalShootTime() - shootEyeShutTime;
+    }
+
+    private void updateAnimation() {
+        string animName = currentAnimationName();
+        var clipToPlay = animator.GetClipByName(animName);
+        if (animator.CurrentClip != clipToPlay) {
+            int frame = animator.CurrentFrame % clipToPlay.frames.Length;
+            animator.PlayFromFrame(clipToPlay, frame);
+        }
+    }
+
+    private string currentAnimationName() {
+        if (isRunning && isShooting && isEyeShut) {
+            return "Shut Shoot Run";
+        }
+        if (isRunning && isShooting) {
+            return "Shoot Run";
+        }
+        if (isRunning) {
+            return "Run";
         }
 
-        if (isFiringAnimationPlaying) {
-            return;
+        if (isShooting && isEyeShut) {
+            return "Shut Shoot Stand";
         }
-
-        if (Mathf.Abs(physics.Vel.x) > 0.1f) {
-            animator.Play("Run");
-        } else {
-            animator.Play("Idle");
+        if (isShooting) {
+            return "Shoot Stand";
         }
+        return "Idle";
     }
 }
